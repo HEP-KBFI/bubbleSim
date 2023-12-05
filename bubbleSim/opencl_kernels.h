db69bac6c36f5450812a8d21d6be66f3fd75567e
Martin Vasar <vasarmartin0@gmail.com>
2023-12-05 11:40:29 +0200

    Cl2 include

--- bubbleSim/opencl_kernels.h
+++ bubbleSim/opencl_kernels.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#define CL_MINIMUM_OPENCL_VERSION 120
+#define CL_TARGET_OPENCL_VERSION 120
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#if __has_include("CL/opencl.hpp")
+#include <CL/opencl.hpp>
+#elif __has_include("CL/cl2.hpp")
+#include <CL/cl2.hpp>
+#else
 #include <CL/cl.hpp>
+#endif
 #pragma comment(lib, "OpenCL.lib")
 
 #include "base.h"
